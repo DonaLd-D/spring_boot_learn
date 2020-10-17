84ad3bdfe700c23e0fce7c54875aa7a9023b93d8
DonaLd-D <57483340+DonaLd-D@users.noreply.github.com>
2020-10-17 22:38:43 +0800

    完成Service和DAO的编写

---
+++ src/main/java/com/imooc/springbootlearn/StudentService.java
@@ -0,0 +1,15 @@
+package com.imooc.springbootlearn;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+    @Autowired
+    StudentMapper studentMapper;
+
+    public Student findStudent(Integer id){
+
+         return studentMapper.findById(id);
+    }
+}
